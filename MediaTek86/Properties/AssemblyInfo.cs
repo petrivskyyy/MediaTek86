84a25a8cb198c2456420e8d7b297c16968ac0bc2
popik <popik@DESKTOP-6AT811G>
2026-06-02 21:50:40 +0200

    Добавьте файлы проекта.

---
+++ MediaTek86/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("MediaTek86")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MediaTek86")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("c0818f6a-f44a-4c98-9318-856787c05ed9")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MediaTek86/bddmanager/BddManager.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+namespace MediaTek86.bddmanager
+{
+    /// <summary>
+    /// Classe singleton permettant la connexion à la base de données
+    /// et l'exécution des requêtes SQL.
+    /// </summary>
+    public class BddManager
+    {
+        /// <summary>
+        /// Instance unique de la classe (singleton).
+        /// </summary>
+        private static BddManager instance = null;
+
+        /// <summary>
+        /// Objet de connexion à la base de données.
+        /// </summary>
+        private readonly MySqlConnection connection;
+
+        /// <summary>
+        /// Constructeur privé pour créer la connexion et l'ouvrir.
+        /// </summary>
+        /// <param name="stringConnect">Chaîne de connexion</param>
+        private BddManager(string stringConnect)
+        {
+            connection = new MySqlConnection(stringConnect);
+            connection.Open();
+        }
+
+        /// <summary>
+        /// Création d'une seule instance de la classe (singleton).
+        /// </summary>
+        /// <param name="stringConnect">Chaîne de connexion</param>
+        /// <returns>L'instance unique de BddManager</returns>
+        public static BddManager GetInstance(string stringConnect)
+        {
+            if (instance == null)
+            {
+                instance = new BddManager(stringConnect);
+            }
+            return instance;
+        }
+
+        /// <summary>
+        /// Exécution d'une requête de type SELECT (lecture).
+        /// </summary>
+        /// <param name="stringQuery">Requête SQL</param>
+        /// <param name="parameters">Paramètres de la requête</param>
+        /// <returns>Liste de tableaux d'objets (les enregistrements)</returns>
+        public List<object[]> ReqSelect(string stringQuery, Dictionary<string, object> parameters = null)
+        {
+            List<object[]> records = new List<object[]>();
+            MySqlCommand command = new MySqlCommand(stringQuery, connection);
+            if (parameters != null)
+            {
+                foreach (KeyValuePair<string, object> parameter in parameters)
+                {
+                    command.Parameters.Add(new MySqlParameter(parameter.Key, parameter.Value));
+                }
+            }
+            command.Prepare();
+            using (MySqlDataReader reader = command.ExecuteReader())
+            {
+                int nbCols = reader.FieldCount;
+                while (reader.Read())
+                {
+                    object[] attributes = new object[nbCols];
+                    reader.GetValues(attributes);
+                    records.Add(attributes);
+                }
+            }
+            return records;
+        }
+
+        /// <summary>
+        /// Exécution d'une requête autre que SELECT (INSERT, UPDATE, DELETE).
+        /// </summary>
+        /// <param name="stringQuery">Requête SQL</param>
+        /// <param name="parameters">Paramètres de la requête</param>
+        public void ReqUpdate(string stringQuery, Dictionary<string, object> parameters = null)
+        {
+            MySqlCommand command = new MySqlCommand(stringQuery, connection);
+            if (parameters != null)
+            {
+                foreach (KeyValuePair<string, object> parameter in parameters)
+                {
+                    command.Parameters.Add(new MySqlParameter(parameter.Key, parameter.Value));
+                }
+            }
+            command.Prepare();
+            command.ExecuteNonQuery();
+        }
+    }
+}
